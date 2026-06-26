5366c7afc029a75ebfe045793f330b260ac148e5
Izharn001 <izharn001@gmail.com>
2026-06-26 17:02:37 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,59 @@
+// import express
+const express = require("express");
+
+// create app
+const app = express();
+
+
+// import dotenv
+const dotenv = require("dotenv");
+
+
+// load dotenv
+dotenv.config();
+
+
+//route
+app.get("/", (req, res) => {
+        res.send('Hello World!');
+});
+
+//secret route
+app.get("/secret", (req,res) => {
+	console.log(req.get("authorization"));
+	const authHeader = req.get("authorization");
+
+	if (!authHeader) {
+		// set WWW- Authenticate header
+		res.set('WWW-Authenticate', 'Basic realm="User Visible Realm"')
+		// set status 401
+		return res.status(401).send("Authentication required");
+
+}
+
+		//decode BasicAuth
+                const encodedCredentials = authHeader.split(" ")[1];
+                const decodedCredentials = Buffer.from(encodedCredentials, "base64").toString("utf-8");
+		const credentials = decodedCredentials.split(":");
+		const username = credentials[0];
+		const password = credentials[1];
+                console.log(username);
+		console.log(password);
+
+
+	if (username === process.env.USERNAME && password === process.env.PASSWORD) {
+		return res.send(process.env.SECRET_MESSAGE);
+}
+		else {
+		res.set('WWW-Authenticate', 'Basic realm="User Visible Realm"')
+                return res.status(401).send("Authentication required");
+}
+
+});
+
+
+
+//listen
+app.listen(3000, () => { 
+        console.log('Example app listening on port 3000!');
+});
